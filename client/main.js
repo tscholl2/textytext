4c288f56b399e8e09b927450a3dd8912d4628180
Travis <tscholl2@uw.edu>
2018-02-19 21:22:36 -0800

    workingo n styling

--- client/main.js
+++ client/main.js
@@ -4,16 +4,27 @@ function mail(to, from, message) {
   return JSON.stringify({ to, from, message });
 }
 
+function parseQueryString(search) {
+  return search
+    .substring(search.indexOf("?") + 1)
+    .split("&")
+    .map(s => s.split("="))
+    .reduce((p, n) => Object.assign(p, { [n[0]]: decodeURIComponent(n[1]) }), {});
+}
+
 function ViewNoName() {
   document.querySelector("header").innerText = `Welcome!`;
-  document.querySelector("main").innerHTML = `
-<label>Name: <input id="input-name" onclick="" type="text" placeholder="A"/></label>
-`;
+  document.getElementById("form-message").classList.toggle("hide");
 }
 
 function ViewName(name) {
   document.querySelector("header").innerText = `Welcome ${name}!`;
-  document.querySelector("main").innerHTML
+  document.getElementById("form-name").style.display = "none";
+}
+
+function updateState(state) {
+  const params = parseQueryString(location.search);
+  return { name: params.name, inbox: state.name === params.name ? state.inbox : [] };
 }
 
 async function start() {
@@ -21,13 +32,43 @@ async function start() {
   const state = {
     name: location.pathname.split("/").pop(),
     inbox: [],
-  }
+  };
+
+  // events
+  document.addEventListener("update-name", event => {
+    const name = event.detail;
+    if (name === state.name) {
+      return;
+    }
+    history.pushState({}, "", `/?name=${name}`);
+    state.name = name;
+  });
+  document.addEventListener("send-message", event => {
+    const to = document.getElementById("input-to").value;
+    const from = state.name;
+    const message = document.getElementById("input-message").value;
+    ws.send(mail(to, from, message));
+  });
+  document.addEventListener("recieve-message", e => {
+    const m = e.detail;
+    if (m.to !== state.name) {
+      return;
+    }
+    state.inbox.push(m);
+  });
+
+  // <DEBUG>
+  state.name = "alice";
+  document.getElementById("form-message").classList.toggle("hide");
+  document.getElementById("form-name").classList.toggle("hide");
+  document.querySelector("header").innerText = `Welcome${
+    state.name !== "" ? " " + state.name : ""
+  }!`;
+  // </DEBUG>
 
   ws.onmessage = e => console.log(`got: ${JSON.stringify(m)}`) || inbox.push(JSON.parse(e.data));
 
   await new Promise(resolve => (ws.onopen = resolve()));
-
-  
 }
 
-start();
+window.onload = start;
