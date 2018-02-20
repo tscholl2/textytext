f6e552e167f1e8295ae9bb7a7d742727cac595f1
Travis <tscholl2@uw.edu>
2018-02-19 16:36:57 -0800

    need to fix seperate websockets pointing to same name

--- client/a.js
+++ client/a.js
@@ -1,12 +1,5 @@
 const WebSocket = require("isomorphic-ws");
 
-/**
- * Formats some mail for passing on wire.
- * @param {string} to
- * @param {string} from
- * @param {string} message
- * @returns {object}
- */
 function mail(to, from, message) {
   return JSON.stringify({ to, from, message });
 }
@@ -14,6 +7,7 @@ function mail(to, from, message) {
 async function main() {
   const A = new WebSocket("ws://localhost:8001/A");
   const B = new WebSocket("ws://localhost:8001/B");
+  const B2 = new WebSocket("ws://localhost:8001/B");
 
   A.onmessage = e => {
     const m = JSON.parse(e.data);
@@ -25,7 +19,12 @@ async function main() {
     console.log(`B recieved mail: ${JSON.stringify(m)}`);
   };
 
-  await Promise.all([A,B].map(P => new Promise(resolve => P.onopen = () => resolve())));
+  B2.onmessage = e => {
+    const m = JSON.parse(e.data);
+    console.log(`B2 recieved mail: ${JSON.stringify(m)}`);
+  };
+
+  await Promise.all([A, B, B2].map(P => new Promise(resolve => (P.onopen = () => resolve()))));
 
   A.send(mail("B", "A", "hello"));
   A.send(mail("B", "A", "hello again"));
