d2b1145d7338c9259c5ff909fafa3a498a8bad0c
Travis Scholl <tscholl2@uw.edu>
2018-02-20 08:21:15 -0800

    initial modal

--- client/main.js
+++ client/main.js
@@ -12,6 +12,151 @@ const state = (initialState = {
   popup: undefined,
 });
 
+// <DEBUG>
+state.name = "ALICE";
+state.inbox = [
+  { to: "ALICE", from: "BOB", message: "hi", date: new Date().toJSON() },
+  { to: "ALICE", from: "BOB", message: "hi again", date: new Date().toJSON() },
+];
+// </DEBUG>
+
+const actions = {
+  onMail: m => () => ({ inbox: inbox.concat([m]) }),
+  form: {
+    onNameChange: e => () => ({ name: e.target.value.toUpperCase() || "" }),
+    onToChange: e => () => ({ to: e.target.value.toUpperCase() || "" }),
+    onMessageChange: e => () => ({ message: e.target.value || "" }),
+  },
+  connect: e => (state, actions) => {
+    e && e.preventDefault();
+    if (state.form.name === state.name) {
+      return;
+    }
+    // connect(name, actions.onMail);
+    return Object.assign({}, initialState, { name: state.form.name });
+  },
+  disconnect: e => (state, actions) => {
+    e && e.preventDefault();
+    return Object.assign({}, initialState, { name: "" });
+  },
+  send: e => state => {
+    e && e.preventDefault();
+    if (state.form.to === "" || state.name === "") {
+      return;
+    }
+    // send(state.form.to, state.name, state.form.message);
+    return { popup: { type: "sent" } };
+  },
+  closeModal: () => () => ({ popup: undefined }),
+};
+
+const MailModal = m => {
+  return h("div", { class: "view-mail" }, [
+    h("p", undefined, `sent at ${m.date}`),
+    h("h3", undefined, m.to),
+    h("h3", undefined, m.from),
+    h("p", undefined, m.message),
+  ]);
+};
+
+const SentModal = () => {
+  return h("div", undefined, [h("p", undefined, "message sent ✓")]);
+};
+
+const view = (state, actions) =>
+  h("div", { id: "app" }, [
+    h("header", undefined, [h("h1", undefined, `WELCOME${state.name ? " " + state.name : ""}!`)]),
+    h("main", undefined, [
+      h(
+        "form",
+        undefined,
+        state.name
+          ? [
+              h("label", undefined, [
+                h("span", undefined, "To: "),
+                h("input", {
+                  type: "text",
+                  placeholder: "BOB",
+                  value: state.form.to,
+                  oninput: actions.form.onToChange,
+                }),
+              ]),
+              h("label", undefined, [
+                h("span", undefined, "Message: "),
+                h("textarea", {
+                  placeholder: "hi",
+                  value: state.form.message,
+                  oninput: actions.form.onMessageChange,
+                }),
+              ]),
+              h("button", { onclick: actions.send }, "ENTER"),
+            ]
+          : [
+              h("label", undefined, [
+                h("span", undefined, "Name: "),
+                h("input", {
+                  type: "text",
+                  placeholder: "ALICE",
+                  value: state.form.name,
+                  oninput: actions.form.onNameChange,
+                }),
+              ]),
+              h("button", { onclick: actions.connect }, "ENTER"),
+            ],
+      ),
+    ]),
+    h(
+      "div",
+      { id: "mailbox" },
+      state.name
+        ? [
+            h("h2", undefined, "Mailbox"),
+            h(
+              "ul",
+              undefined,
+              state.inbox.map(m =>
+                h("li", undefined, [
+                  h("div", { class: "mail" }, [h("span", undefined, `From: ${m.from}`)]),
+                ]),
+              ),
+            ),
+          ]
+        : undefined,
+    ),
+    h("footer", undefined, [
+      h(
+        "p",
+        undefined,
+        "Your message will show up right after it is sent. Once shown, your message is deleted.",
+      ),
+      h("a", { href: "https://git.tws.website/t/snaptext" }, "source"),
+      h("a", { href: "#", onclick: actions.disconnect }, "Signout"),
+    ]),
+    state.popup
+      ? h("div", { id: "modal", class: "modal" }, [
+          h("div", { class: "modal-content" }, [
+            h("span", { class: "modal-close", onclick: actions.closeModal }, "×"),
+            // fill in
+            (() => {
+              switch (state.popup.type) {
+                case "mail":
+                  return MailModal(state.popup.props, state, actions);
+                case "sent":
+                  return SentModal(state.popup.props, state, actions);
+                default:
+                  return undefined;
+              }
+            })(),
+            h("p", undefined, "some text..."),
+          ]),
+        ])
+      : undefined,
+  ]);
+
+const main = app(state, actions, view, document.body);
+
+// helper functions
+
 let ws;
 function connect(name = "", listener) {
   if (ws) {
@@ -21,9 +166,8 @@ function connect(name = "", listener) {
   if (!name) {
     return;
   }
-  ws = new WebSocket(`ws://${WEBSOCKET_HOST}/${name}`);
-  ws.onmessage = e => listener(JSON.parse(e.data));
-  history.pushState(null, "", `/?name=${name}`);
+  // ws = new WebSocket(`ws://${WEBSOCKET_HOST}/${name}`);
+  // ws.onmessage = e => listener(JSON.parse(e.data));
 }
 
 function send(to, from, message) {
@@ -31,80 +175,6 @@ function send(to, from, message) {
     ws.send(JSON.stringify({ to, from, message }));
   }
 }
-
-const actions = {
-  onMail: m => () => ({ inbox: inbox.concat([m]) }),
-  form: {
-    onNameChange: e => state =>
-      console.log("state = ", state) ||
-      console.log("new value = ", e.target.value) || { name: e.target.value.toUpperCase() || "" },
-    onToChange: e => () => ({ to: e.target.value.toUpperCase() || "" }),
-    onMessageChange: e => () => ({ message: e.target.value || "" }),
-  },
-  connect: e => (state, actions) => {
-    e && e.preventDefault();
-    connect(state.form.name, actions.onMail);
-    return Object.assign({}, initialState, { name: state.form.name });
-  },
-  send: e => state => {
-    e && e.preventDefault();
-    if (state.form.to === "" || state.name === "") {
-      return;
-    }
-    send(state.form.to, state.name, state.form.message);
-    return { popup: "sent" };
-  },
-};
-
-const view = (state, actions) =>
-  h("div", { id: "app" }, [
-    h("header", undefined, `WELCOME${state.name ? " " + state.name : ""}!`),
-    h("main", undefined, [
-      state.name
-        ? h("form", undefined, [
-            h("label", undefined, [
-              h("span", undefined, "To: "),
-              h("input", {
-                type: "text",
-                placeholder: "BOB",
-                value: state.form.to,
-                oninput: actions.form.onToChange,
-              }),
-            ]),
-            h("label", undefined, [
-              h("span", undefined, "Message: "),
-              h("textarea", {
-                placeholder: "hi",
-                value: state.form.message,
-                oninput: actions.form.onMessageChange,
-              }),
-            ]),
-            h("button", { onclick: actions.send }, "ENTER"),
-          ])
-        : h("form", undefined, [
-            h("label", undefined, [
-              h("span", undefined, "Name: "),
-              h("input", {
-                type: "text",
-                placeholder: "ALICE",
-                value: state.form.name,
-                oninput: actions.form.onNameChange,
-              }),
-            ]),
-            h("button", { onclick: actions.connect }, "ENTER"),
-          ]),
-    ]),
-    h("footer", undefined, [
-      h(
-        "p",
-        undefined,
-        "Your message will show up right after it is sent. Once shown, your message is deleted.",
-      ),
-      h("a", { href: "https://git.tws.website/t/snaptext" }, "source"),
-    ]),
-  ]);
-
-const main = app(state, actions, view, document.body);
 
 function parseQueryString(search = "") {
   return search
